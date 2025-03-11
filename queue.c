bc94e347ac3ba201e6fc67f5212b5ba6154913b0
poyu <dean.xu.2357@gmail.com>
2025-03-11 14:46:58 +0800

    Fix buffer overflow risk in string copy operations
    Modify q_remove_head and q_remove_tail functions to ensure proper null
    termination after strncpy operations. This prevents potential buffer
    overflows by:
    - Limiting copy to 'bufsize-1' characters
    - Explicitly adding null terminator at the last position
    
    Change-Id: I7821de1c95d4c16b98e3d32ba7a24271751d4b6c

--- queue.c
+++ queue.c
@@ -88,7 +88,8 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
     element_t *h = list_first_entry(head, element_t, list);
     list_del_init(&h->list);
 
-    strncpy(sp, h->value, bufsize);
+    strncpy(sp, h->value, bufsize - 1);
+    sp[bufsize - 1] = '\0';
     return h;
 }
 
@@ -104,7 +105,8 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
     element_t *t = list_last_entry(head, element_t, list);
     list_del_init(&t->list);
 
-    strncpy(sp, t->value, bufsize);
+    strncpy(sp, t->value, bufsize - 1);
+    sp[bufsize - 1] = '\0';
     return t;
 }
 
